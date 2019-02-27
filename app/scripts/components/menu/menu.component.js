dfb3a2c7f256d2e8686c8814f5062584b904f895
Martin Campagnoli <mcampagnoli@canto.com>
2019-02-27 17:41:50 +0100

    Add menu functionality

--- app/scripts/components/menu/menu.component.js
+++ app/scripts/components/menu/menu.component.js
@@ -10,21 +10,33 @@
       controller: Controller,
     });
 
-  Controller.$inject = ['$rootScope'];
+  Controller.$inject = ['$anchorScroll'];
     /* @ngInject */
-  function Controller($rootScope)
+  function Controller($anchorScroll)
   {
     var ctrl = this;
+    ctrl.menu = [
+      {name: "START", hash:"home", active: true},
+      {name: "COUNTRY", hash:"countryInfo", active: false},
+      {name: "WHAT TO SEE", hash:"whattosee", active: false},
+      {name: "ABOUT", hash:"about", active: false}
+    ];
 
     ctrl.$onInit = function() {
-
-      // $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
-      // var $anchor = $(this);
-      // $('html, body').stop().animate({
-      // scrollTop: $($anchor.attr('href')).offset().top
-      // }, 1200, 'easeInOutExpo');
-      // event.preventDefault();
-      // });
+      ctrl.clear = clear;
+      ctrl.goTo = goTo;
     };
+
+    function clear(){
+      angular.forEach(ctrl.menu, function(elem){
+        elem.active = false;
+      });
+    }
+
+    function goTo(item){
+      item.active = true;
+      $anchorScroll(item.hash);
+    }
+
   }
 })();
